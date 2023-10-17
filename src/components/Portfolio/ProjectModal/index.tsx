54b0882a288628f43ad7b34fd579097180948ade
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2023-10-17 19:23:28 -0300

    Carousel with modal for projects

--- src/components/Portfolio/ProjectModal/index.tsx
+++ src/components/Portfolio/ProjectModal/index.tsx
@@ -1,101 +1,103 @@
-import React from 'react'
+'use client';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import portfImgs from '@/assets/imgs/portfolio';
 
-export default function ProjectModal() {
+import './styles.css';
+
+type Props = {
+  projectTitle: string;
+  imgsUrls: string[];
+};
+
+export default function ProjectModal({ projectTitle, imgsUrls }: Props) {
+  const [projectImgs, setProjectImgs] = useState<any[]>([]);
+  useEffect(() => {
+    const imgs: any[] = [];
+    imgsUrls &&
+      imgsUrls.forEach(item => {
+        imgs.push(require('@/assets/imgs/projects' + item.substring(1)));
+      });
+    setProjectImgs(imgs);
+  }, [imgsUrls]);
+
   return (
-      <div
-        className="modal fade"
-        id="projectModal"
-        tabIndex={-1}
-        aria-labelledby="projectModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-body">
+    <div
+      className="modal fade"
+      id="projectModal"
+      tabIndex={-1}
+      aria-labelledby="projectModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog modal-dialog-centered modal-xl">
+        <div className="modal-content">
+          <div className="modal-body">
+            <div className="btn-close-ctr">
               <button
                 type="button"
                 className="btn-close"
                 data-bs-dismiss="modal"
                 aria-label="Close"
               ></button>
+            </div>
 
-              {/* Carousel */}
-              <div id="projectCarousel" className="carousel slide">
-                <div className="carousel-indicators">
-                  <button
-                    type="button"
-                    data-bs-target="#projectCarousel"
-                    data-bs-slide-to="0"
-                    className="active"
-                    aria-current="true"
-                    aria-label="Slide 1"
-                  ></button>
-                  <button
-                    type="button"
-                    data-bs-target="#projectCarousel"
-                    data-bs-slide-to="1"
-                    aria-label="Slide 2"
-                  ></button>
-                  <button
-                    type="button"
-                    data-bs-target="#projectCarousel"
-                    data-bs-slide-to="2"
-                    aria-label="Slide 3"
-                  ></button>
-                </div>
-                <div className="carousel-inner">
-                  <div className="carousel-item active">
-                    <Image
-                      src={portfImgs.p2}
-                      alt=""
-                      className="d-block w-100 h-auto"
-                    />
-                  </div>
-                  <div className="carousel-item">
-                    <Image
-                      src={portfImgs.p3}
-                      alt=""
-                      className="d-block w-100 h-auto"
-                    />
-                  </div>
-                  <div className="carousel-item">
-                    <Image
-                      src={portfImgs.p4}
-                      alt=""
-                      className="d-block w-100 h-auto"
-                    />
-                  </div>
-                </div>
-                <button
-                  className="carousel-control-prev"
-                  type="button"
-                  data-bs-target="#projectCarousel"
-                  data-bs-slide="prev"
-                >
-                  <span
-                    className="carousel-control-prev-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span className="visually-hidden">Previous</span>
-                </button>
-                <button
-                  className="carousel-control-next"
-                  type="button"
-                  data-bs-target="#projectCarousel"
-                  data-bs-slide="next"
-                >
-                  <span
-                    className="carousel-control-next-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span className="visually-hidden">Next</span>
-                </button>
+            {/* Carousel */}
+            <div id="projectCarousel" className="carousel slide">
+              <div className="carousel-indicators">
+                {projectImgs &&
+                  projectImgs.map((img, index) => (
+                    <button
+                      key={Math.random()}
+                      type="button"
+                      data-bs-target="#projectCarousel"
+                      data-bs-slide-to={`${index}`}
+                      className="active"
+                      aria-current="true"
+                      aria-label={`Slide ${index + 1}`}
+                    ></button>
+                  ))}
               </div>
+
+              <div className="carousel-inner">
+                {projectImgs &&
+                  projectImgs.map((img, index) => (
+                    <div key={Math.random()} className="carousel-item active">
+                      <Image
+                        src={img}
+                        alt={`${projectTitle} - ${index + 1}`}
+                        className="d-block w-100 h-auto"
+                      />
+                    </div>
+                  ))}
+              </div>
+
+              <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target="#projectCarousel"
+                data-bs-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target="#projectCarousel"
+                data-bs-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Next</span>
+              </button>
             </div>
           </div>
         </div>
       </div>
-  )
+    </div>
+  );
 }
--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,24 +1,36 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import portfImgs from '@/assets/imgs/portfolio';
 import ProjectModal from './ProjectModal';
 import getImgsFromFolder from '@/assets/imgs/projects';
 
 import './styles.css';
 
 export default function Portfolio() {
-  const [projectsImgs, setProjectsImgs] = useState<any[]>([]);
+  const [projectsUrls, setProjectsUrls] = useState<any>({});
+  const [projectsFirstImg, setProjectsFirstImg] = useState<any[]>([]);
+  const [projectTitles, setProjectTitles] = useState<string[]>([]);
+  const [modalChoice, setModalChoice] = useState(projectTitles[0] || '');
+
   useEffect(() => {
     const projects: any = getImgsFromFolder();
+
+    setProjectsUrls(projects);
+    setProjectTitles(Object.keys(projects));
+
     const firstImgUrlFromEachFolder: string[] = [];
     for (let key in projects) {
-      firstImgUrlFromEachFolder.push( require(
-        '@/assets/imgs/projects' + projects[key].at(0).substring(1))
+      firstImgUrlFromEachFolder.push(
+        require('@/assets/imgs/projects' + projects[key].at(0).substring(1)),
       );
     }
-    setProjectsImgs(firstImgUrlFromEachFolder);
+    setProjectsFirstImg(firstImgUrlFromEachFolder);
   }, []);
+
+  function handleImgClick(index: number, event: any): void {
+    event.preventDefault();
+    setModalChoice(projectTitles[index]);
+  }
 
   return (
     <section className="portfolio" id="portfolio">
@@ -32,50 +44,38 @@ export default function Portfolio() {
         </div>
         <div className="portfolio-content">
           <div className="row justify-content-center">
-            <div className="col-lg-4 col-md-6">
-
-              {projectsImgs &&
-                projectsImgs.map(item => (
-                  <div className="project_box" key={Math.random()}>
+            {projectsFirstImg &&
+              projectsFirstImg.map((item, index) => (
+                <div className="col-lg-4 col-md-6" key={index}>
+                  <div className="project_box">
                     <div className="single_project">
                       <Image
                         src={item}
-                        alt=""
+                        alt={projectTitles[index]}
                         className="img-fluid w-100"
                         data-bs-toggle="modal"
                         data-bs-target="#projectModal"
+                        onClick={event => handleImgClick(index, event)}
                       />
                     </div>
                     <div className="short_info">
-                      <h4>
-                        <a href="#">minimal design</a>
-                      </h4>
+                      <h4>{projectTitles[index]}</h4>
                     </div>
                   </div>
-                ))}
-
-              <div className="project_box">
-                <div className="single_project">
-                  <Image
-                    src={portfImgs.p1}
-                    alt=""
-                    className="img-fluid w-100"
-                    data-bs-toggle="modal"
-                    data-bs-target="#projectModal"
-                  />
                 </div>
-                <div className="short_info">
-                  <h4>
-                    <a href="#">minimal design</a>
-                  </h4>
-                </div>
-              </div>
-
-            </div>
+              ))}
           </div>
         </div>
       </div>
-      <ProjectModal />
+
+      {projectsUrls &&
+        Object.keys(projectsUrls).map((_item, index) => (
+          <ProjectModal
+            key={index}
+            projectTitle={modalChoice}
+            imgsUrls={projectsUrls[modalChoice]}
+          />
+        ))}
     </section>
   );
 }
