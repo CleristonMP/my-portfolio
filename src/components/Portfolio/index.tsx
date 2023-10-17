ca08a8c4f8968ce84f098911f0ad605e03bf64d2
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2023-10-17 12:09:07 -0300

    Dynamically rendering project images

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,12 +1,25 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import portfImgs from '@/assets/imgs/portfolio';
 import ProjectModal from './ProjectModal';
+import getImgsFromFolder from '@/assets/imgs/projects';
 
 import './styles.css';
 
 export default function Portfolio() {
+  const [projectsImgs, setProjectsImgs] = useState<any[]>([]);
+  useEffect(() => {
+    const projects: any = getImgsFromFolder();
+    const firstImgUrlFromEachFolder: string[] = [];
+    for (let key in projects) {
+      firstImgUrlFromEachFolder.push( require(
+        '@/assets/imgs/projects' + projects[key].at(0).substring(1))
+      );
+    }
+    setProjectsImgs(firstImgUrlFromEachFolder);
+  }, []);
+
   return (
     <section className="portfolio" id="portfolio">
       <div className="container">
@@ -20,6 +33,27 @@ export default function Portfolio() {
         <div className="portfolio-content">
           <div className="row justify-content-center">
             <div className="col-lg-4 col-md-6">
+
+              {projectsImgs &&
+                projectsImgs.map(item => (
+                  <div className="project_box" key={Math.random()}>
+                    <div className="single_project">
+                      <Image
+                        src={item}
+                        alt=""
+                        className="img-fluid w-100"
+                        data-bs-toggle="modal"
+                        data-bs-target="#projectModal"
+                      />
+                    </div>
+                    <div className="short_info">
+                      <h4>
+                        <a href="#">minimal design</a>
+                      </h4>
+                    </div>
+                  </div>
+                ))}
+
               <div className="project_box">
                 <div className="single_project">
                   <Image
@@ -36,6 +70,7 @@ export default function Portfolio() {
                   </h4>
                 </div>
               </div>
+
             </div>
           </div>
         </div>
